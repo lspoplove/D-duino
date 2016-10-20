096a1ffe5547e92244231415b0109d64cf8ce7c7
Dongsen Technology <lspoplove@naver.com>
2016-10-20 17:08:56 +0800

    Update Webtoboard.ino

--- Arduino-examples/Webtoboard/Webtoboard.ino
+++ Arduino-examples/Webtoboard/Webtoboard.ino
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2015, Majenko Technologies
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -8,8 +37,8 @@ SSD1306  display(0x3c, D1, D2);
 WiFiClient client;
 String data = "";
 String tempIP = "";
-const char *ssid = "Your WiFi";
-const char *password = "Your Password";
+const char *ssid = "BWSAG";
+const char *password = "bshg@rfda";
 
 ESP8266WebServer server ( 80 );
 
@@ -86,6 +115,26 @@ void loop ( void ) {
 	server.handleClient();
   display.drawString(0, 0,"WiFi connected"); 
   display.drawString(0, 10,tempIP); 
-  display.drawString(0, 20, data);   
-  display.display(); 
+ // display.drawString(0, 20, data);
+
+  if(data.length()<21){
+  display.drawString(0, 20, data);
+  }
+  else if(21<data.length()<41){
+  display.drawString(0, 20, data.substring(0,21));
+  display.drawString(0, 30, data.substring(21,data.length()));
+  }
+  else if(42<data.length()<63){
+  display.drawString(0, 20, data.substring(0,21));
+  display.drawString(0, 30, data.substring(21,42));
+  display.drawString(0, 40, data.substring(42,data.length()));
+  }
+  else if(63<data.length()<84){
+  display.drawString(0, 20, data.substring(0,21));
+  display.drawString(0, 30, data.substring(21,42));
+  display.drawString(0, 40, data.substring(42,63));
+  display.drawString(0, 50, data.substring(63,data.length()));
+  }
+ 
+display.display(); 
 }
